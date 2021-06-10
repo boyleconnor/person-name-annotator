d2ecb3669bde4ee3f4265e0c03dbabcf8505a7df
Connor Boyle <connor.bo@gmail.com>
2021-06-09 18:29:36 -0700

    Add load_data Module

---
+++ server/model/load_data.py
@@ -0,0 +1,41 @@
+import os
+from typing import Any
+from xml.etree import ElementTree
+from xml.etree.ElementTree import Element
+
+
+Annotation = dict[str, Any]
+AnnotationSet = list[Annotation]
+
+
+def load_annotation(tag: Element) -> Annotation:
+    annotation = dict(tag.items())
+
+    # Convert index values to integers
+    for key in ('start', 'end'):
+        annotation[key] = int(annotation[key])
+
+    return annotation
+
+
+def load_file(path: str) -> (str, AnnotationSet):
+    tree = ElementTree.parse(path)
+    text: str = tree.find('TEXT').text
+    tags: Element = tree.find('TAGS')
+
+    annotations = []
+    for tag in tags:
+        annotations.append(load_annotation(tag))
+
+    return text, annotations
+
+
+def load_directory(path: str) -> (list[str], list[AnnotationSet]):
+    '''Load a directory of I2B2 PHI data (in XML)
+    '''
+    texts, annotation_sets = [], []
+    for filename in os.listdir(path):
+        text, annotation_set = load_file(os.path.join(path, filename))
+        texts.append(text)
+        annotation_sets.append(annotation_set)
+    return texts, annotation_sets
