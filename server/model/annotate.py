f09eb28d521ff266bf5b14877d477bd04bd2436d
Connor Boyle <connor.bo@gmail.com>
2021-06-16 16:51:36 -0700

    Annotate Texts

--- server/model/annotate.py
+++ server/model/annotate.py
@@ -1,10 +1,9 @@
-from typing import Union, BinaryIO
-
 import joblib
 import numpy as np
+from typing import Union, BinaryIO, List
 from sklearn.linear_model import SGDClassifier
-
 from model.label_spans import label_spans
+from model.load_data import AnnotationSet
 from model.tokenize import Tokenizer
 from model.vectorize import Vectorizer
 
@@ -15,7 +14,7 @@ class Annotator:
         self.model = SGDClassifier()
         self.vectorizer = Vectorizer()
 
-    def train(self, texts, annotation_sets):
+    def train(self, texts: List[str], annotation_sets: List[AnnotationSet]):
         # Turn texts + annotations into (sequences of) tokens + labels
         token_sequences, span_sequences, label_sequences = [], [], []
         for text, annotation_set in zip(texts, annotation_sets):
@@ -35,10 +34,38 @@ class Annotator:
         # Train model
         self.model.fit(x, y)
 
-    def annotate(self, text: str):
+    def annotate(self, texts: List[str]) -> List[AnnotationSet]:
         """Create annotations for a given text
         """
-        raise NotImplementedError
+        token_sequences, span_sequences = [], []
+        for text in texts:
+            tokens, spans = self.tokenizer.get_tokens_and_spans(text)
+            token_sequences.append(tokens)
+            span_sequences.append(spans)
+
+        # Get input vectors
+        x = self.vectorizer.transform(token_sequences)
+
+        # Get predictions
+        y = self.model.predict(x)
+
+        i = 0
+        annotation_sets = []
+        for tokens, spans in zip(token_sequences, span_sequences):
+            annotation_set = []
+            for token, span in zip(tokens, spans):
+                # If the token has been marked, create an annotation for it
+                if y[i] == 1:
+                    start, end = span
+                    annotation_set.append({
+                        'text': token,
+                        'start': start,
+                        'end': end
+                    })
+                i += 1
+            annotation_sets.append(annotation_set)
+
+        return annotation_sets
 
     def save(self, file: Union[str, BinaryIO]):
         """Save annotator to file (either path-string or file-object).
