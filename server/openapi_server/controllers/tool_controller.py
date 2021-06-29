11ae4dc69ccfccddf506ba342e14303e75bf8858
Connor Boyle <connor.bo@gmail.com>
2021-06-28 19:25:13 -0700

    Fix Tool Description

--- server/openapi_server/controllers/tool_controller.py
+++ server/openapi_server/controllers/tool_controller.py
@@ -16,7 +16,7 @@ def get_tool():  # noqa: E501
         version="0.1.8",
         license=License.APACHE_2_0,
         repository="github:cascadianblue/person-name-annotator",
-        description="Connor Boyle'",
+        description="ML based annotator by Connor Boyle",
         author="Connor Boyle",
         author_email="connorbo@gmail.com",
         url="https://github.com/cascadianblue/person-name-annotator",
