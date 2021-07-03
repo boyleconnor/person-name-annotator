ce92b61cf220b63cc5a34153cdfbc898457d1275
Connor Boyle <connor.bo@gmail.com>
2021-07-02 23:23:19 -0700

    Fix Tokenizer to Handle Empty Notes

--- server/openapi_server/annotator/tokenize.py
+++ server/openapi_server/annotator/tokenize.py
@@ -10,6 +10,11 @@ class Tokenizer:
     def get_tokens_and_spans(self, text: str) -> Tuple[Tokens, Spans]:
         """Get a list of tokens & a list of labels (one for each token)
         """
+        # Handle empty or all-whitespace notes
+        if text == '' or text.isspace():
+            return [''], [(0, 0)]
+
+        # Handle non-empty notes
         tokens = self.tokenizer.tokenize(text)
         spans = self.tokenizer.span_tokenize(text)
 
